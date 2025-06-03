11f0e14933f63e373f22696dd482edd83ea0de58
Tony Mantana <anton.butorin11@yandex.ru>
2025-06-03 18:34:59 +0300

    Added a new test

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -31,3 +31,25 @@ func TestGetLanguageTypeByName(t *testing.T) {
 		})
 	}
 }
+
+func TestGetLanguageNameByType(t *testing.T) {
+	tCases := []struct {
+		programmLanguageType types.Language
+		expected             string
+	}{
+		{types.Language(0), "go"},
+		{types.Language(1), "php"},
+		{types.Language(2), "js"},
+	}
+	for _, tc := range tCases {
+		t.Run(tc.expected, func(t *testing.T) {
+			res, err := GetLanguageNameByType(tc.programmLanguageType)
+			if err != nil {
+				t.FailNow()
+			}
+			if res != tc.expected {
+				t.Errorf("GetLanguageNameByType returned %v, expected %v", res, tc.expected)
+			}
+		})
+	}
+}
